2e8813db92b6ad18151c45429bf3d02b92daebf1
zhekacher <zhekacher20.05@gmail.com>
2019-07-01 17:18:07 +0600

    IT DOESN'T WORK

--- Gimmick_Website/settings.py
+++ Gimmick_Website/settings.py
@@ -120,9 +120,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'website/static',
     'chat/static',
-    'auth/static'
+    'auth/static',
+    'news/static'
 ]
--- website/views.py
+++ website/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, cache_control
 
 from database.bot_data_schema import general_values_table
 from database.data_schema import users_table, news_table
 from database.database_connection import Database
 
 database = Database()
-
+telegram_chat_bot_link = general_values_table.get_value(database.get_connection(), "CHAT_BOT_LINK")
+telegram_scanner_bot_link = general_values_table.get_value(database.get_connection(), "SCANNER_BOT_LINK")
+telegram_chat_link = general_values_table.get_value(database.get_connection(), "CHAT_LINK")
 
 # @cache_page(600, cache='default', key_prefix='')
+@cache_control(no_cache=True)
 def index(request):
     # while not connection.is_connected():
     #     connection = database_connection.connect()
     #     time.sleep(5000)
     username = ''
-    telegram_chat_bot_link = general_values_table.get_value(database.get_connection(), "CHAT_BOT_LINK")
-    telegram_scanner_bot_link = general_values_table.get_value(database.get_connection(), "SCANNER_BOT_LINK")
-    telegram_chat_link = general_values_table.get_value(database.get_connection(), "CHAT_LINK")
     telegram_id = ''
     main_news = news_table.get_last_high_priority_news(database.get_connection())
     secondary_news = news_table.get_three_last_news(database.get_connection(), main_news.news_id)
